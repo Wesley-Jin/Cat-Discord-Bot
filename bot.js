4f4b599ead5855fb6a665c1827a35552deeb27b0
Wesley <jw95097@hotmail.com>
2022-05-26 03:44:01 +1000

    Add another set of cat images and discord command

--- bot.js
+++ bot.js
@@ -23,6 +23,14 @@ client.on("message", msg => {
     }
 })
 
+client.on("message", msg => {
+    if (msg.content === `${BOT_PREFIX}jacky`) {
+        customCatImage(msg);
+        msg.react("💖")
+        msg.react("🐱‍👤")
+    }
+})
+
 client.on('error', data => {
     console.log('error', data);
 })
@@ -67,50 +75,114 @@ async function getCatImage(sub_id) {
         console.log(error);
     }
     return response;
-  
-    
-    // const imageNumber = Math.floor(Math.random() * 98) + 1;
-    
-    // switch (true) {
-    //     // wallpaperhouse URL 327005 has no image, added another image instead
-    //     case (imageNumber === 5):
-    //         msg.channel.send('Error link! Here\'s Doudou instead 💖');
-    //         msg.channel.send('https://i.imgur.com/lMsc4re.png');
-    //     case (imageNumber < 10):
-    //         msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_32700${imageNumber}.jpg`);
-    //         break;
-    //     case (imageNumber <= 54):
-    //         msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_3270${imageNumber}.jpg`);
-    //         break;
-    //     case (imageNumber <= 57):
-    //         msg.channel.send('https://i.imgur.com/NOp9vG8.jpg');
-    //         break;
-    //     case (imageNumber <= 60):
-    //         msg.channel.send('https://i.imgur.com/lMsc4re.png');
-    //         break;
-    //     case (imageNumber <= 63):
-    //         msg.channel.send('https://i.imgur.com/F5UPvYa.jpg');
-    //         break;
-    //     case (imageNumber <= 66):
-    //         msg.channel.send('https://i.imgur.com/EOcrkTO.jpg');
-    //         break;    
-    //     case (imageNumber <= 69):
-    //         msg.channel.send('https://i.imgur.com/pJNqeZu.jpg');
-    //         break;
-    //     case (imageNumber <= 72):
-    //         msg.channel.send('https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80');
-    //         break;
-    //     case (imageNumber <= 75):
-    //         msg.channel.send('https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80');
-    //         break;    
-    //     case (imageNumber <= 78):
-    //         msg.channel.send('https://images.unsplash.com/photo-1566847438217-76e82d383f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80');
-    //         break;
-    //     case (imageNumber <= 99):
-    //         msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_3269${imageNumber}.jpg`);
-    //         break;
-    //     default:
-    //         msg.channel.send('Bad value');
-    //         break;
-    // }
 }
+
+    function customCatImage(msg) {
+    const imageNumber = Math.floor(Math.random() * 17) + 1;
+    
+    switch (true) {
+        case (imageNumber === 1):
+            msg.channel.send('https://i.imgur.com/He9oSkX.jpeg');
+            break;
+        case (imageNumber === 2):
+            msg.channel.send('https://i.imgur.com/YbTdVby.jpeg');
+            break;
+        case (imageNumber === 3):
+            msg.channel.send('https://i.imgur.com/3HcNFGy.jpeg');
+            break;
+        case (imageNumber === 4):
+            msg.channel.send('https://i.imgur.com/Hd2yUty.jpeg');
+            break;
+        case (imageNumber === 5):
+            msg.channel.send('https://i.imgur.com/FII02Ap.jpeg');
+            break;
+        case (imageNumber === 6):
+            msg.channel.send('https://i.imgur.com/GcCmIPZ.jpeg');
+            break;
+        case (imageNumber === 7):
+            msg.channel.send('https://i.imgur.com/I7tqrvj.jpeg');
+            break;
+        case (imageNumber === 8):
+            msg.channel.send('https://i.imgur.com/QTtiq3q.jpeg');
+            break;
+        case (imageNumber === 9):
+            msg.channel.send('https://i.imgur.com/im636M3.jpeg');
+            break;
+        case (imageNumber === 10):
+            msg.channel.send('https://i.imgur.com/SqPSVXv.jpeg');
+            break;
+        case (imageNumber === 11):
+            msg.channel.send('https://i.imgur.com/nF4j6vl.jpeg');
+            break;
+        case (imageNumber === 12):
+            msg.channel.send('https://i.imgur.com/FmpwUVV.jpeg');
+            break;
+        case (imageNumber === 13):
+            msg.channel.send('https://i.imgur.com/ynqrmk2.jpeg');
+            break;
+        case (imageNumber === 14):
+            msg.channel.send('https://i.imgur.com/iNtlMDk.jpeg');
+            break;
+        case (imageNumber === 15):
+            msg.channel.send('https://i.imgur.com/AGnGRUn.jpeg');
+            break;
+        case (imageNumber === 16):
+            msg.channel.send('https://i.imgur.com/PB9eqBp.jpeg');
+            break;
+        case (imageNumber === 17):
+            msg.channel.send('https://i.imgur.com/qkBoYZM.jpeg');
+            break;
+        case (imageNumber === 18):
+            msg.channel.send('https://i.imgur.com/yFC1uD6.jpeg');
+            break;
+        default:
+            msg.channel.send('Bad value, Wesley is dumb, yikes 🤡');
+            break;
+    }
+}
+
+/*
+  const imageNumber = Math.floor(Math.random() * 98) + 1;
+    
+    switch (true) {
+        // wallpaperhouse URL 327005 has no image, added another image instead
+        case (imageNumber === 5):
+            msg.channel.send('Error link! Here\'s Doudou instead 💖');
+            msg.channel.send('https://i.imgur.com/lMsc4re.png');
+        case (imageNumber < 10):
+            msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_32700${imageNumber}.jpg`);
+            break;
+        case (imageNumber <= 54):
+            msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_3270${imageNumber}.jpg`);
+            break;
+        case (imageNumber <= 57):
+            msg.channel.send('https://i.imgur.com/NOp9vG8.jpg');
+            break;
+        case (imageNumber <= 60):
+            msg.channel.send('https://i.imgur.com/lMsc4re.png');
+            break;
+        case (imageNumber <= 63):
+            msg.channel.send('https://i.imgur.com/F5UPvYa.jpg');
+            break;
+        case (imageNumber <= 66):
+            msg.channel.send('https://i.imgur.com/EOcrkTO.jpg');
+            break;    
+        case (imageNumber <= 69):
+            msg.channel.send('https://i.imgur.com/pJNqeZu.jpg');
+            break;
+        case (imageNumber <= 72):
+            msg.channel.send('https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80');
+            break;
+        case (imageNumber <= 75):
+            msg.channel.send('https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80');
+            break;    
+        case (imageNumber <= 78):
+            msg.channel.send('https://images.unsplash.com/photo-1566847438217-76e82d383f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80');
+            break;
+        case (imageNumber <= 99):
+            msg.channel.send(`https://wallpaper-house.com/data/out/9/wallpaper2you_3269${imageNumber}.jpg`);
+            break;
+        default:
+            msg.channel.send('Bad value');
+            break;
+    }*/
